47eefad396cebc57fa486e061eca92f50e330dd4
Umair <umair.khalid786@outlook.com>
2023-02-12 15:02:55 +0500

    Fonts introduced

--- app/src/main/java/com/techlads/composetvkeyboard/theme/Type.kt
+++ app/src/main/java/com/techlads/composetvkeyboard/theme/Type.kt
@@ -2,33 +2,44 @@ package com.techlads.composetvkeyboard.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = hind_light,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = hind_regular,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.0.sp,
+        letterSpacing = 0.2.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = hind_regular,
+        fontWeight = FontWeight.Thin,
+        fontSize = 12.sp,
+        lineHeight = 16.0.sp,
+        letterSpacing = 0.4.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = hind_light,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = hind_light,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
 )
