4196985a5557b4abd7c6b23137dcc05447cd066a
Umair <umair.khalid786@outlook.com>
2023-02-12 23:40:03 +0500

    Keyboard action callback added

--- app/src/main/java/com/techlads/composetvkeyboard/utilities/key_extensions.kt
+++ app/src/main/java/com/techlads/composetvkeyboard/utilities/key_extensions.kt
@@ -7,6 +7,7 @@ import com.techlads.composetvkeyboard.domain.model.UtilityKey
 
 fun Key.isBackspace() = this is UtilityKey.Backspace || this is NumericUtilityKey.Backspace
 fun Key.isUppercase() = this is UtilityKey.Uppercase
+fun Key.isAction() = this is UtilityKey.ActionArrow
 fun Key.isNumeric() = this is TextUtilityKey.Numeric
 fun Key.isAbc() = this is TextUtilityKey.ABC
 fun Key.isClear() = this is UtilityKey.Clear
